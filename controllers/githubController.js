9aa02a13bfbe7e4cc003a4483c25064c6e4e36bb
Mani <kodali.mani092@gmail.com>
2026-05-30 11:59:50 +0530

    Initial commit

---
+++ controllers/githubController.js
@@ -0,0 +1,97 @@
+const axios = require("axios");
+const db = require("../config/db");
+
+const analyzeProfile = async (req, res) => {
+    try {
+        const username = req.params.username;
+
+        const response = await axios.get(
+            `https://api.github.com/users/${username}`
+        );
+
+        const user = response.data;
+
+        const sql = `
+        INSERT INTO github_profiles
+        (username, name, followers, following, public_repos, profile_url)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+        followers = ?,
+        following = ?,
+        public_repos = ?,
+        profile_url = ?`;
+
+        db.query(
+            sql,
+            [
+                user.login,
+                user.name,
+                user.followers,
+                user.following,
+                user.public_repos,
+                user.html_url,
+
+                user.followers,
+                user.following,
+                user.public_repos,
+                user.html_url
+            ],
+            (err) => {
+                if (err) {
+                    return res.status(500).json(err);
+                }
+
+                res.json({
+                    message: "Profile analyzed and stored successfully",
+                    data: user
+                });
+            }
+        );
+
+    } catch (error) {
+        res.status(404).json({
+            message: "GitHub user not found"
+        });
+    }
+};
+
+const getAllProfiles = (req, res) => {
+    db.query(
+        "SELECT * FROM github_profiles",
+        (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            res.json(result);
+        }
+    );
+};
+
+const getSingleProfile = (req, res) => {
+    const username = req.params.username;
+
+    db.query(
+        "SELECT * FROM github_profiles WHERE username = ?",
+        [username],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            if (result.length === 0) {
+                return res.status(404).json({
+                    message: "Profile not found"
+                });
+            }
+
+            res.json(result[0]);
+        }
+    );
+};
+
+module.exports = {
+    analyzeProfile,
+    getAllProfiles,
+    getSingleProfile
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const githubRoutes = require("./routes/githubRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/github", githubRoutes);
+
+app.get("/", (req, res) => {
+    res.send("GitHub Profile Analyzer API Running");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
