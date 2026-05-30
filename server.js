f8f0747a2ade6dd5a4821fc80a3976b5591e1313
Mani <kodali.mani092@gmail.com>
2026-05-30 12:34:40 +0530

    Add environment variables for deployment

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.get("/", (req, res) => {
     res.send("GitHub Profile Analyzer API Running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
